da03d0e48c059286a31e292c8f93d1fd97a7ccbe
梁仕翘 <liangshiqiao@corp.netease.com>
2019-07-29 13:41:23 +0800

    创建窗体，编写设置界面。

---
+++ TheLibrary/TheLibrary/Window/PictureReaderSetting.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TheLibrary.Window
+{
+    /// <summary>
+    /// PictureReaderSetting.xaml 的交互逻辑
+    /// </summary>
+    public partial class PictureReaderSetting
+    {
+        public PictureReaderSetting()
+        {
+            InitializeComponent();
+            m_WheelPaging.IsChecked = true;
+            m_WheelZoom.IsChecked = false;
+        }
+
+        private void M_WheelZoom_Checked(object sender, RoutedEventArgs e)
+        {
+            m_WheelPaging.IsChecked = !m_WheelZoom.IsChecked;
+        }
+
+        private void M_WheelPaging_Checked(object sender, RoutedEventArgs e)
+        {
+            m_WheelZoom.IsChecked = !m_WheelPaging.IsChecked;
+        }
+    }
+}
